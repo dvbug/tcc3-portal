57e707483f4e4fb142005591e4fa3feab6d67319
Vito2015 <w_yeer9988@126.com>
2015-12-16 10:31:23 +0800

    update config

--- tcc3portal/settings_override.py
+++ tcc3portal/settings_override.py
@@ -15,12 +15,12 @@ MONGODB_SETTINGS = {
     'port': 20000
 }
 
-DAC_HOST_URL = "192.168.1.118:8080"
+DAC_HOST_URL = 'dac.tcc.org'  # '192.168.1.118:8080'
 DAC_API_CONFIGS_LINES_URL = '/api/v1.0/configs/lines'
 DAC_API_SCHEDULES_URL = '/api/v1.0/schedules/'
 
 
-SSO_CENTER_HOST = '192.168.1.118:9001'
+SSO_CENTER_HOST = 'sso.tcc.org'  # '192.168.1.118:9001'
 SSO_CENTER_URL = 'http://'+SSO_CENTER_HOST
 SSO_CENTER_REGISTER_URL = 'http://'+SSO_CENTER_HOST+'/sso/register'
 SSO_CENTER_REFERRER_KEY = 'url'
